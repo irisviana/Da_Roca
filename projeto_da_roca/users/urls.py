9bb0e611bb5ddb4d63e762c8b7eecb7dee493fd2
vinesnts <v.santos0406@gmail.com>
2021-05-20 00:22:38 -0300

    Adicionada rota de deletar horario de entrega

--- projeto_da_roca/users/urls.py
+++ projeto_da_roca/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cadastro_cliente/', cadastro_cliente, name='cadastro_cliente'),
     path('horarioentrega/listar', HorarioEntregaView.listarHorarioEntrega, name='horarioentrega-listar'),
     path('horarioentrega/cadastro', HorarioEntregaView.criarHorarioEntrega, name='horarioentrega-cadastro'),
+    path('horarioentrega/deletar', HorarioEntregaView.deletarHorarioEntrega, name='horarioentrega-deletar'),
 ]
