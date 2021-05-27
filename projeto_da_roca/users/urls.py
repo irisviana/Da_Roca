5bbd75db4aef7b66b1402b4fd796b92cca06ea88
irisviana <vianasantana21@gmail.com>
2021-05-27 18:35:19 -0300

    adicionando rota e pastas de admin

--- projeto_da_roca/users/urls.py
+++ projeto_da_roca/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import list_users, create_users, DeliveryTimeView, ServiceAddressView
+from .views import list_users, create_users, DeliveryTimeView, ServiceAddressView,admin_home,list_admin,add_admin
 
 urlpatterns = [
     path('', list_users),
@@ -12,4 +12,7 @@ urlpatterns = [
     path('service_address/list', ServiceAddressView.list_service_address, name='list_service_address'),
     path('service_address/create', ServiceAddressView.create_service_address, name='create_service_address'),
     path('service_address/delete', ServiceAddressView.delete_service_address, name='delete_service_address'),
+    path('admin/', admin_home, name='home_admin'),
+    path('admin/manage_admin', list_admin, name='manage_admin'),
+    path('admin/add', add_admin, name='admin_add'),
 ]
