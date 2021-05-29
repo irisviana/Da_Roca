95570f8c7a5289f18c6762b77142e0c9c0734945
matheus <matheus.noronha@solyd.com.br>
2021-05-28 23:57:55 -0300

    added infos from smtp server

--- projeto_da_roca/Da_Roca/settings.py
+++ projeto_da_roca/Da_Roca/settings.py
@@ -149,3 +149,13 @@ AUTHENTICATION_BACKENDS = ['users.backends.EmailBackend']
 LOGIN_REDIRECT_URL = ''
 LOGIN_URL = 'login/'
 LOGOUT_REDIRECT_URL = ''
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rocroca49@gmail.com'
+EMAIL_HOST_PASSWORD = 'daroca1234'
+
--- projeto_da_roca/users/urls.py
+++ projeto_da_roca/users/urls.py
@@ -3,17 +3,24 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # User account
     path('', UserView.list_users),
     path('create', UserView.create_users, name='create_customer'),
     path('<str:username>/update', UserView.update_users, name='update_customer'),
+
+    # Address
     path('<str:username>/address/create', AddressView.create_address, name='create_customer_address'),
     path('<str:username>/address/list', AddressView.list_address, name='list_customer_address'),
+
+    # Delivery Time
     path('delivery_time/list/<int:service_address_id>', DeliveryTimeView.list_delivery_time, name='list_delivery_time'),
     path('delivery_time/create/<int:service_address_id>', DeliveryTimeView.create_delivery_time,
          name='create_delivery_time'),
     path('delivery_time/delete/', DeliveryTimeView.delete_delivery_time, name='delete_delivery_time'),
     path('delivery_time/update/<int:delivery_time_id>', DeliveryTimeView.update_delivery_time,
          name='update_delivery_time'),
+
+    # ServiceAddress
     path('service_address/list', ServiceAddressView.list_service_address, name='list_service_address'),
     path('service_address/create', ServiceAddressView.create_service_address, name='create_service_address'),
     path('service_address/delete', ServiceAddressView.delete_service_address, name='delete_service_address'),
