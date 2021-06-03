12fb34fad2ce17f1a26040d02a3c163a0b72889e
irisviana <vianasantana21@gmail.com>
2021-06-02 21:54:36 -0300

    criando app de produtos e model de produtos

---
+++ projeto_da_roca/products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class Product(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    variety = models.CharField(max_length=200, null=True, blank=True)
+    expiration_days = models.IntegerField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    stock_amount = models.IntegerField(null=True, blank=True)
+    
+
