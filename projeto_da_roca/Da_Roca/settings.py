4fbac89b294e9e022c222ee4f66ca95a0c8498cc
matheus <matheus.noronha@solyd.com.br>
2021-06-10 22:43:08 -0300

    reset password

--- projeto_da_roca/Da_Roca/settings.py
+++ projeto_da_roca/Da_Roca/settings.py
@@ -154,7 +154,7 @@ LOGOUT_REDIRECT_URL = ''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rocroca49@gmail.com'
 EMAIL_HOST_PASSWORD = 'daroca1234'
--- projeto_da_roca/Da_Roca/urls.py
+++ projeto_da_roca/Da_Roca/urls.py
@@ -12,9 +12,17 @@ urlpatterns = [
     path('logout/', logout_page, name='logout'),
     path('admin/', admin.site.urls),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name='registration/reset_password.html'),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_password_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset_password_form.html'),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/reset_password_done.html'),
+         name='password_reset_complete'),
 
 ]
