76ad2738313901be4fa046277062c115397d5ed1
Íris <iris@pop-os.localdomain>
2021-07-08 19:53:11 -0300

    updating settings

--- Da_Roca/settings.py
+++ Da_Roca/settings.py
@@ -24,9 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -108,6 +105,8 @@ if os.getenv('BUILD_ON_TRAVIS', None):
         }
     }
 else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = env('SECRET_KEY')
     #Non-travis DB configuration goes here
     DATABASES = {
         'default': {
